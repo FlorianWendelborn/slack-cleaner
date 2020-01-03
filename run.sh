42887b7b79e51816c6c4ceae3a1ce95d200a3198
Georg vom Endt <gve@3yourmind.com>
2020-01-03 23:47:13 +0100

    fix(using-real-wendelshell):being smart about mac
    Why:
     * fix ugly
    
    How:
     * use fancy variable
     * test for OSTYPE

--- run.sh
+++ run.sh
@@ -9,38 +9,40 @@ DAYS_TO_LEAVE="${1:-14}"
 NAMES_TO_DELETE="$(cat names.txt)"
 DATEUTIL="$(which gdate || which date)"
 
+shift
+
 if test -z "$SLACK_TOKEN"; then
-    echo "SLACK_TOKEN not provided. Aborting."
-    exit 1
+	echo "SLACK_TOKEN not provided. Aborting."
+	exit 1
 fi
 
 if test -z "$SLACK_USERNAME"; then
-    echo "SLACK_USERNAME not provided. Aborting."
-    exit 1
+	echo "SLACK_USERNAME not provided. Aborting."
+	exit 1
 fi
 
 if test -z "$NAMES_TO_DELETE"; then
-    echo "names.txt is empty. Nothing to do here."
-    exit 0
+	echo "names.txt is empty. Nothing to do here."
+	exit 0
 fi
 
 ### SCRIPT
 
-# macbros: run `brew install coreutils` and use `gdate`
-DATE=$(
-    $DATEUTIL \
-        --date=@$(($($DATEUTIL +%s) - $DAYS_TO_LEAVE * 86400)) \
-        +%Y%m%d
-)
+
+if [[ $OSTYPE == *linux* ]]; then
+	DATE=$(date --date=@$(( $(date +%s) - $DAYS_TO_LEAVE * 86400 )) +%Y%m%d)
+elif [[ $OSTYPE == *darwin* ]]; then
+	DATE=$(date -r $(( $(date +%s) - $DAYS_TO_LEAVE * 86400 )) +%Y%m%d)
+fi
 
 source ./venv/bin/activate
 echo "add --perform to actually do the thing"
 
 for NAME in $NAMES_TO_DELETE; do
-    slack-cleaner \
-        --token "$SLACK_TOKEN" \
-        --rate=1 --message \
-        --direct "$NAME" \
-        --user "$SLACK_USERNAME" \
-        --before "$DATE" --log $@
+	slack-cleaner \
+		--token "$SLACK_TOKEN" \
+		--rate=1 --message \
+		--direct "$NAME" \
+		--user "$SLACK_USERNAME" \
+		--before "$DATE" --log $@
 done
